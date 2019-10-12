57a1eb1d092e614c8a563bf44738587708ded13c
Wesley Matos <wesley.matos@redfox.tech>
2019-10-12 15:18:05 -0300

    remove create user router

--- src/modules/users/routes.js
+++ src/modules/users/routes.js
@@ -1,16 +1,12 @@
 const { Router } = require('express');
-const { auth, create } = require('./controller');
+const { auth } = require('./controller');
 const contracts = require('./contract');
 const validator = require('../../middlewares/validator');
-const checkPermission = require('../../middlewares/checkPermission');
-const authM = require('../../middlewares/auth');
 
 module.exports = () => {
   const router = Router();
 
   router.post('/auth', validator(contracts.auth, 'body'), auth);
-
-  router.post('/create', authM, checkPermission, validator(contracts.create, 'body'), create);
 
   return { router, endpoint: '/user' };
 };
