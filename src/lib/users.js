1d15c9c2e7e1d35d611741e92b12957a212198fa
Wesley Matos <wesley.matos@redfox.tech>
2019-10-14 03:30:29 -0300

    add new users

--- src/lib/users.js
+++ src/lib/users.js
@@ -5,4 +5,16 @@ module.exports = [
     password: '$2a$13$zhjQMa78SaO7cPBGbnFnNO2UNHL7BrxjcfCUYQsxwvTwcMtWX1hyq',
     isAdmin: true,
   },
+  {
+    name: 'Icaro Caldeira',
+    email: 'icaro.caldeira@rankmyapp.com.br',
+    password: '$2a$13$wEdFcUokIb3zKybunosY7OJNSokKOkydGC2ssK9nffexHNq4DYYJi',
+    isAdmin: true,
+  },
+  {
+    name: 'Gabriela Spricigo',
+    email: 'gabriela.spricigo@rankmyapp.com.br',
+    password: '$2a$13$AyX0MvIBZV2XzOZJ9879D.ifVokJ3LSI9VCJOHefRRAUd0kRO239S',
+    isAdmin: true,
+  },
 ];
